melobern/ohmyzsh

commit 09d2a597a588abd69647c37b082009125ae07b9a

Fix style of colored-man-pages plugin

plugins/colored-man-pages/colored-man-pages.plugin.zsh
@@ -1,32 +1,32 @@
-if [ "$OSTYPE[0,7]" = "solaris" ]
+if [ "$OSTYPE" = solaris* ]
 then
-	if [ ! -x ${HOME}/bin/nroff ]
+	if [ ! -x "$HOME/bin/nroff" ]
 	then
-		mkdir -p ${HOME}/bin
-		cat > ${HOME}/bin/nroff <<EOF
+		mkdir -p "$HOME/bin"
+		cat > "$HOME/bin/nroff" <<EOF
 #!/bin/sh
 if [ -n "\$_NROFF_U" -a "\$1,\$2,\$3" = "-u0,-Tlp,-man" ]; then
 	shift
-	exec /usr/bin/nroff -u\${_NROFF_U} "\$@"
+	exec /usr/bin/nroff -u\$_NROFF_U "\$@"
 fi
 #-- Some other invocation of nroff
 exec /usr/bin/nroff "\$@"
 EOF
-	chmod +x ${HOME}/bin/nroff
+		chmod +x "$HOME/bin/nroff"
 	fi
 fi
 
 man() {
-      env \
-      	  LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-	  LESS_TERMCAP_md=$(printf "\e[1;31m") \
-	  LESS_TERMCAP_me=$(printf "\e[0m") \
-	  LESS_TERMCAP_se=$(printf "\e[0m") \
-	  LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-	  LESS_TERMCAP_ue=$(printf "\e[0m") \
-	  LESS_TERMCAP_us=$(printf "\e[1;32m") \
-	  PAGER=/usr/bin/less \
-	  _NROFF_U=1 \
-	  PATH="$HOME/bin:$PATH" \
-	  			   man "$@"
+	env \
+		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+		LESS_TERMCAP_md=$(printf "\e[1;31m") \
+		LESS_TERMCAP_me=$(printf "\e[0m") \
+		LESS_TERMCAP_se=$(printf "\e[0m") \
+		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+		LESS_TERMCAP_ue=$(printf "\e[0m") \
+		LESS_TERMCAP_us=$(printf "\e[1;32m") \
+		PAGER=/usr/bin/less \
+		_NROFF_U=1 \
+		PATH="$HOME/bin:$PATH" \
+			man "$@"
 }

commit 15ff38d921e75f0fdaaaf7d2af0bdbbb42d0387b

Fix syntax error in colored-man-pages
Commit 09d2a597a5 introduced the bug, which is due to using
single brackets conditions at the same time as *.

This commit should fix it and use double brackets everywhere
else.

Fixes #4699

plugins/colored-man-pages/colored-man-pages.plugin.zsh
@@ -1,6 +1,6 @@
-if [ "$OSTYPE" = solaris* ]
+if [[ "$OSTYPE" = solaris* ]]
 then
-	if [ ! -x "$HOME/bin/nroff" ]
+	if [[ ! -x "$HOME/bin/nroff" ]]
 	then
 		mkdir -p "$HOME/bin"
 		cat > "$HOME/bin/nroff" <<EOF